cc7db4b2f00599e84742520c33f5107b73c033a5
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-09-02 09:30:26 +0600

    some changes added

--- js/app.js
+++ js/app.js
@@ -7,31 +7,47 @@ const errorMsg = document.getElementById('error');
 // load books data
 const loadBooks = () => {
     const searchText = searchField.value;
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    // error handling
     if (searchField.value === '') {
-        errorMsg.innerText = 'Please enter a book name!'
+        errorMsg.innerHTML = `
+            <h5 class="text-center w-25 mx-auto p-3 bg-danger text-white">
+                Please enter a book name!
+            </h5>`;
+        booksContainer.innerHTML = '';
+        results.innerText = '';
         return;
     }
     fetch(url)
         .then(res => res.json())
         .then(data => displayBooks(data.docs))
+        // clear input field
         .finally(() => searchField.value = '')
 }
 
+// displaying search results/books.
 const displayBooks = (books) => {
-    results.innerText = `${books.length}`
+    // number of total search results.
+    results.innerText = `Total search results: ${books.length}`;
+    errorMsg.innerHTML = '';
+    booksContainer.innerHTML = '';
+    // error handling
     if (books.length === 0) {
-        errorMsg.innerText = 'No Results Found!'
+        errorMsg.innerHTML = `
+            <h5 class="text-center w-25 mx-auto p-3 bg-danger text-white">
+                No Results Found!
+            </h5>`;
+        results.innerText = '';
     }
+    // single result/book
     books.forEach(book => {
-
         const { title, publisher, author_name, first_publish_year, cover_i } = book;
-        const imgUrl = `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`
+        const imgUrl = `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`;
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-            <img src="${imgUrl}" class="card-img-top h-50 img-fluid" alt="book image">
+            <img src="${imgUrl}" class="card-img-top h-75 img-fluid" alt="book image">
             <div class="card-body">
                 <h4 class="card-title">${title}</h4>
                 <h6 class="card-text">By: ${author_name}</h6>
