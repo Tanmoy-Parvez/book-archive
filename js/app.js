a9630fd0fba7bfee6643e1a7e1a3a8a75e3602da
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-09-02 12:01:15 +0600

    some changes added

--- js/app.js
+++ js/app.js
@@ -3,42 +3,43 @@ const searchField = document.getElementById('input-field');
 const results = document.getElementById('results');
 const errorMsg = document.getElementById('error');
 
+// spinner
 const toggleSpinner = displayStyle => {
     document.getElementById('spinner').style.display = displayStyle;
 }
 
-// load books data
-const loadBooks = () => {
-    const searchText = searchField.value;
+// clear previous elements
+const removeElements = () => {
     error.innerHTML = '';
     booksContainer.innerHTML = '';
     results.innerText = '';
-    toggleSpinner('block');
+}
 
-    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+// load books data
+const loadBooks = () => {
+    removeElements(); //clear elements
+    toggleSpinner('block'); //add spinner
+    const searchText = searchField.value;
     // error handling
     if (searchField.value === '') {
-        toggleSpinner('none');
+        toggleSpinner('none');  //remove spinner
         errorMsg.innerHTML = `
-            <h5 class="text-center w-25 mx-auto p-3 bg-danger text-white">
-                Please enter a book name!
-            </h5>`;
+        <h5 class="text-center w-25 mx-auto p-3 bg-danger text-white">
+            Please enter a book name!
+        </h5>`;
         return;
     }
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayBooks(data.docs))
-        // clear input field
-        .finally(() => searchField.value = '')
+        .finally(() => searchField.value = '') //clear input field
 }
 
-// displaying search results/books.
+// displaying search books.
 const displayBooks = (books) => {
     toggleSpinner('none');
-    // number of total search results.
-    results.innerText = `Total search results: ${books.length}`;
-    errorMsg.innerHTML = '';
-    booksContainer.innerHTML = '';
+    results.innerText = `Total search results: ${books.length}`; //number of total search results.
     // error handling
     if (books.length === 0) {
         toggleSpinner('none');
@@ -48,9 +49,9 @@ const displayBooks = (books) => {
             </h5>`;
         results.innerText = '';
     }
-    // single result/book
+    // single book
     books.forEach(book => {
-        const { title, publisher, author_name, first_publish_year, cover_i } = book;
+        const { title, publisher, author_name, first_publish_year, cover_i } = book; //destructuring
         const imgUrl = `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`;
         const div = document.createElement('div');
         div.classList.add('col');
