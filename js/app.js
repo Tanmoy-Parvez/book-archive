deea62a176d9e596284f597268ad8aedcd116acd
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-09-02 15:34:00 +0600

    some changes added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// capturing all elements by id
 const booksContainer = document.getElementById('books-container');
 const searchField = document.getElementById('input-field');
 const results = document.getElementById('results');
@@ -36,10 +37,10 @@ const loadBooks = () => {
         .finally(() => searchField.value = '') //clear input field
 }
 
-// displaying search books.
+// displaying searched books.
 const displayBooks = (books) => {
     toggleSpinner('none');
-    results.innerText = `Total search results: ${books.length}`; //number of total search results.
+    results.innerText = `Total Search Results: ${books.length}`; //number of total search results.
     // error handling
     if (books.length === 0) {
         toggleSpinner('none');
@@ -47,22 +48,24 @@ const displayBooks = (books) => {
             <h5 class="text-center  w-25 mx-auto p-3 bg-danger text-white">
                 NO RESULTS FOUND!
             </h5>`;
-        results.innerText = '';
+        results.innerText = ''; //clear previous result
     }
     // single book
     books.forEach(book => {
+        console.log(book);
         const { title, publisher, author_name, first_publish_year, cover_i } = book; //destructuring
         const imgUrl = `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`;
         const div = document.createElement('div');
-        div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100">
-            <img src="${imgUrl}" class="card-img-top h-75 img-fluid" alt="book image">
-            <div class="card-body">
-                <h4 class="card-title">${title}</h4>
-                <h6 class="card-text">By: ${author_name}</h6>
-                <p class="card-text">Publisher: ${publisher[0]}</p>
-                <p class="card-text">First published in ${first_publish_year}</p>
+        <div class="col h-75">
+            <div class="card h-100">
+                <img src="${imgUrl}" class="card-img-top h-50 p-1" alt="book image">
+                <div class="card-body overflow-auto">
+                    <h4 class="card-title">${title.slice(0, 30)}</h4>
+                    <h6 class="card-text"><strong>By:</strong> ${author_name ? author_name : 'no name found'}</h6>
+                    <h6 class="card-text"><strong>Publisher:</strong> ${publisher ? publisher[0] : 'no name found'}</h6>
+                    <h6 class="card-text">First published in <strong>${first_publish_year ? first_publish_year : '...'}</strong></h6>
+                </div>
             </div>
         </div>
         `;
